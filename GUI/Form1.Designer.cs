ebcf2de52b95df17741cd9a6be57af7e7fe08289
AnugrahSA <100453661+AnugrahSA@users.noreply.github.com>
2022-03-04 17:46:29 +0800

    Add files via upload
    fitur tambahan GUI

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.nama = new System.Windows.Forms.TextBox();
+            this.hasil = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(240, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SUBMIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nama
+            // 
+            this.nama.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nama.Location = new System.Drawing.Point(240, 127);
+            this.nama.Name = "nama";
+            this.nama.Size = new System.Drawing.Size(237, 38);
+            this.nama.TabIndex = 1;
+            this.nama.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // hasil
+            // 
+            this.hasil.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hasil.Location = new System.Drawing.Point(240, 275);
+            this.hasil.Name = "hasil";
+            this.hasil.Size = new System.Drawing.Size(237, 38);
+            this.hasil.TabIndex = 1;
+            this.hasil.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hasil);
+            this.Controls.Add(this.nama);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nama;
+        private System.Windows.Forms.TextBox hasil;
+    }
+}
+
